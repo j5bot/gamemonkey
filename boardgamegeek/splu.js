c25dc46670bbe19027943a8ecaca6c2a4a25f933
Jonathan 'J5' Cook <jonathan.j5.cook@gmail.com>
2025-10-06 09:49:36 -0400

    added userscript version of bookmarklet SPLU script for logging plays

---
+++ boardgamegeek/splu.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name         BGG SPLU Userscript Version
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  improves the play logging experience on BGG
+// @author       dazeysan, j5bot
+// @match        https://boardgamegeek.com/boardgame/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    document.SPLUcode = '';
+    document.SPLUxml = '';
+
+    function addSPLU () {
+        const SPLUscript = document.createElement('script');
+        SPLUscript.type = 'text/javascript';
+        SPLUscript.src = 'https://dazeysan.github.io/SPLU/Source%20Code/SPLU-Current.js';
+        document.body.appendChild(SPLUscript);
+    }
+
+    setTimeout(() => {
+        const logPlayButtons = Array.from(document.querySelectorAll('button[play-recorder-button]'));
+
+        logPlayButtons.forEach(logPlayButton => {
+            const spluButton = document.createElement(logPlayButton.tagName);
+            spluButton.className = logPlayButton.className;
+            spluButton.innerHTML = logPlayButton.innerHTML;
+            spluButton.onclick = function () {
+                if (window.initSPLU !== undefined) {
+                    window.initSPLU();
+                } else {
+                    try {
+                        addSPLU();
+                    } catch (e) {}
+                }
+            };
+
+            logPlayButton.style.display = 'none';
+            logPlayButton.parentElement.insertBefore(spluButton, logPlayButton);
+        });
+    }, 1000);
+})();
