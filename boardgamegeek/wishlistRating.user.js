c5b8efba19e238349b48fdf8bd992d5f83899380
Jonathan 'J5' Cook <jonathan.j5.cook@gmail.com>
2026-05-03 12:09:29 -0400

    update wishlist-as-rating

--- boardgamegeek/wishlistRating.user.js
+++ boardgamegeek/wishlistRating.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         BGG Wishlist Emoji in Rating Column
 // @namespace    http://github.com/j5bot/gamemonkey
-// @version      0.1
+// @version      0.2
 // @author       j5bot
 // @description  Add an emoji representing the wishlist priority to the rating column
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=boardgamegeek.com
 // @match        https://boardgamegeek.com/collection/*
+// @match        https://boardgamegeek.com/browse/boardgame
 // @downloadURL  https://raw.githubusercontent.com/j5bot/gamemonkey/refs/heads/main/boardgamegeek/wishlistRating.user.js
 // @updateURL    https://raw.githubusercontent.com/j5bot/gamemonkey/refs/heads/main/boardgamegeek/wishlistRating.user.js
 // ==/UserScript==
@@ -25,8 +26,14 @@
             const rating = wish.parentElement.parentElement.parentElement.querySelector('.rating');
             const node = (rating.firstElementChild ?? rating);
             if (node.innerText === 'N/A') {
-                node.innerText = emoji;
-                node.style.fontSize = '26px';
+                if (priority === 5) {
+                    rating.style.backgroundColor = 'black';
+                    rating.style.color = 'white';
+                    node.innerText = `N/I ${emoji}`;
+                } else {
+                    node.innerText = emoji;
+                    node.style.fontSize = '26px';
+                }
             } else {
                 node.innerHTML = `${emoji} ${node.innerHTML}`;
                 node.style.fontSize = '20px';
